8c79db15c181976e7d6bbd30274dc028dc2ca0cf
martinmexico3@gmail.com <martinmexico3@gmail.com>
2023-03-10 18:42:07 +0800

    Initial commit

---
+++ test_-_hx711.ino
@@ -0,0 +1,29 @@
+#include "HX711.h"
+
+// HX711 circuit wiring
+const int LOADCELL_DOUT_PIN = 3;
+const int LOADCELL_SCK_PIN = 4;
+float calibration_factor = 1000;
+
+HX711 scale;
+
+void setup() {
+  Serial.begin(9600);
+  scale.begin(LOADCELL_DOUT_PIN, LOADCELL_SCK_PIN);
+  scale.set_scale(calibration_factor);
+  scale.tare(); //Reset the scale to 0
+}
+
+void loop() {
+
+  if (scale.is_ready()) {
+    long reading = scale.get_units();
+    Serial.print("HX711 reading: ");
+    Serial.println(reading);
+  } else {
+    Serial.println("HX711 not found.");
+  }
+
+  delay(1000);
+  
+}
